26f3ac000ffe2de860be66c337fb5008542e3986
ritesh-gade <riteshgade16@gmail.com>
2024-02-14 18:10:21 +0530

    Change

--- vars/qualityGate.groovy
+++ vars/qualityGate.groovy
@@ -1,3 +1,3 @@
 def call(credentialsId) {
-    waitForQualityGate abortPipeline: false, credentialsId: 'Sonar-token'  
+    waitForQualityGate abortPipeline: false, credentialsId: credentialsId   
 }
